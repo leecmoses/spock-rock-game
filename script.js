17e270948ffdb4efae999856a5d21ceeea455309
moses <leecmoses@gmail.com>
2022-07-26 10:42:37 -0400

    Write computer choice selection logic

--- script.js
+++ script.js
@@ -28,14 +28,66 @@ const choices = {
   spock: { name: "Spock", defeats: ["scissors", "rock"] },
 };
 
+let computerChoice = "";
+
 // Reset all 'selected' icons
-
-
-// Passing player selection value and styling icons
-const select = (playerChoice) => {
+const resetSelected = () => {
   allGameIcons.forEach((icon) => {
     icon.classList.remove("selected");
   });
+};
+
+const computerRandomChoice = () => {
+  const computerChoiceNumber = Math.random();
+  if (computerChoiceNumber <= 0.2) {
+    computerChoice = "rock";
+  } else if (computerChoiceNumber <= 0.4) {
+    computerChoice = "paper";
+  } else if (computerChoiceNumber <= 0.6) {
+    computerChoice = "scissors";
+  } else if (computerChoiceNumber <= 0.8) {
+    computerChoice = "lizard";
+  } else {
+    computerChoice = "spock";
+  }
+};
+
+// Add 'selected' styling & computerChoice
+const displayComputerChoice = (computerChoice) => {
+  switch (computerChoice) {
+    case "rock":
+      computerRock.classList.add("selected");
+      computerChoiceEl.textContent = " --- Rock";
+      break;
+    case "paper":
+      computerPaper.classList.add("selected");
+      computerChoiceEl.textContent = " --- Paper";
+      break;
+    case "scissors":
+      computerScissors.classList.add("selected");
+      computerChoiceEl.textContent = " --- Scissors";
+      break;
+    case "lizard":
+      computerLizard.classList.add("selected");
+      computerChoiceEl.textContent = " --- Lizard";
+      break;
+    case "spock":
+      computerSpock.classList.add("selected");
+      computerChoiceEl.textContent = " --- Spock";
+      break;
+  }
+};
+
+// Call functions to process turn
+const checkResult = () => {
+  resetSelected();
+  computerRandomChoice();
+  displayComputerChoice(computerChoice);
+};
+
+// Passing player selection value and styling icons
+const select = (playerChoice) => {
+  checkResult();
   switch (playerChoice) {
     case "rock":
       playerRock.classList.add("selected");
